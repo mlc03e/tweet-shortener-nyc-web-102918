c0b4711226b4b99285f43f417eb278ba6ba02c85
Meghan Campbell <mlc03e@gmail.com>
2018-10-22 23:42:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,6 +3,8 @@ def dictionary(hash)
 end
 
 def word_substituter(string)
+    dictionary = { "hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
+  
   string.each do |word|
     if word == dictionary.keys
       replace 

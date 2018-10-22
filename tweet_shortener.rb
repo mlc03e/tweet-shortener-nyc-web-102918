b450053434cb7ba5e1c03294b81cad32ebc3b2d3
Meghan Campbell <mlc03e@gmail.com>
2018-10-22 23:38:41 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
 def dictionary(hash)
-  dictionary = { "hello" => "hi", "to, two, too" => "2", "for, four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
+  dictionary = { "hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
 end
 
 def word_substituter(string)

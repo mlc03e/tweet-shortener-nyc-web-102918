05343b1a63d2e7e759429d8db2bd13832c98dce1
Meghan Campbell <mlc03e@gmail.com>
2018-10-22 23:30:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,2 +1,2 @@
 def dictionary(hash)
-  dictionary = { "hello" => "hi", "to, two, too" => "2", 
+  dictionary = { "hello" => "hi", "to, two, too" => "2", "for, four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&" }
